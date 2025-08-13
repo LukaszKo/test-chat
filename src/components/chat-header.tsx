5391e04937dadc9da330ecb344b59e8f4de07cd7
Łukasz Kochajewski <lukaszkochajewski@gmail.com>
2025-08-13 22:54:04 +0200

    wip

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs))
+}
+
 
 interface ChatHeaderProps {
   title: string;
@@ -7,8 +14,8 @@ interface ChatHeaderProps {
 
 const ChatHeader: React.FC<ChatHeaderProps> = ({ title }) => {
   return (
-    <View className={'bg-white px-4 py-4 border-b border-gray-100'}>
-      <Text className={'text-2xl font-bold text-gray-900'}>{title}</Text>
+    <View className={cn('bg-white px-4 py-4 border-b border-gray-100')}>
+      <Text className={cn('text-4xl font-bold text-gray-900')}>{title}</Text>
     </View>
   );
 };
